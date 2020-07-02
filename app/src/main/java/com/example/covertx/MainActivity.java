0a7b85f9926c4d45cee1f589fb4039783add2fb8
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2020-07-02 15:27:36 +0530

    rupee to dollar converter

---
+++ app/src/main/java/com/example/covertx/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.covertx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+        EditText rupeeAmt = findViewById(R.id.inputId);
+        String amount = rupeeAmt.getText().toString();
+        double dollarAmt = 70 * Double.parseDouble(amount);
+        String toastText = String.valueOf(dollarAmt);
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
